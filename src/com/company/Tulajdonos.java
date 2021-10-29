c157511461ded4e8c175d90b75b36af79817723f
Poós Dániel <poosdani@gmail.com>
2021-10-29 14:17:12 +0200

    initial commit

---
+++ src/com/company/Tulajdonos.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public final class Tulajdonos {
+    private String nev;
+
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+}
